4e5707215994de9a6df1aa70fc8de1b2af33e857
RudenkoArt <rudnkoart@gmail.com>
2023-01-18 15:13:56 +0200

    ready to deploy

---
+++ blog/src/actions/index.js
@@ -0,0 +1,42 @@
+import _ from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach((id) => dispatch(fetchPosts(id)))
+    .value();
+};
+
+export const fetchPosts = () => async (dispatch) => {
+  const { data } = await jsonPlaceholder.get('/posts');
+
+  dispatch({
+    type: 'FETCH_POSTS',
+    payload: data,
+  });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const { data } = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({
+    type: 'FETCH_USER',
+    payload: data,
+  });
+};
+
+// memoized version using lodash
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const { data } = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({
+//     type: 'FETCH_USER',
+//     payload: data,
+//   });
+// });
--- blog/src/index.js
+++ blog/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { legacy_createStore as createStore } from 'redux';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
+const store = createStore(reducers, applyMiddleware(thunk));
+
 const root = createRoot(document.querySelector('#root'));
 root.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>
 );
