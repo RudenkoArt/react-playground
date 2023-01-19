37981aa49281d10080f820fdcf7e6ea83187f0da
RudenkoArt <rudnkoart@gmail.com>
2023-01-19 15:46:37 +0200

    started to work with redux-form library

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+} from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
+const composeEnhencers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhencers(applyMiddleware()));
 const root = createRoot(document.querySelector('#root'));
-root.render(<App />);
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
