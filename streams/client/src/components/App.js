069abd987275a0a01c3332c4d979bbaf3b0c97c8
RudenkoArt <rudnkoart@gmail.com>
2023-01-19 10:31:11 +0200

    create stream components

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+  return (
+    <div>
+      <Link to='/pagetwo'>Navigate to page 2</Link>
+      Page 1
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      <Link to='/'>Navigate to page 1</Link>
+      Page 2<button>Click Me!</button>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path='/' exact component={PageOne} />
+          <Route path='/pagetwo' component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
