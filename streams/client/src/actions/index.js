a7f8b5d8c215f0221eafd30fec89999f8221ff7a
RudenkoArt <rudnkoart@gmail.com>
2023-01-20 15:45:27 +0200

    refactor StreamCreate.js

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -27,6 +28,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
   const { data } = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: data });
+  history.push('/');
 };
 
 export const fetchStreams = () => async (dispatch) => {
@@ -42,9 +44,10 @@ export const fetchStream = (id) => async (dispatch) => {
 };
 
 export const editStream = (id, formValues) => async (dispatch) => {
-  const { data } = await streams.put(`/steams/${id}`, formValues);
+  const { data } = await streams.put(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: data });
+  history.push('/');
 };
 
 export const deleteStream = (id) => async (dispatch) => {
