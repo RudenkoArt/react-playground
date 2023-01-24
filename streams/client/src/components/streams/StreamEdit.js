6e62d12a85fc683a2ef05ef2764682bdf0058060
RudenkoArt <rudnkoart@gmail.com>
2023-01-24 09:48:52 +0200

    refactor StreamForm using react-final-form

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -17,7 +17,7 @@ class StreamEdit extends React.Component {
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
-    console.log(this.props);
+
     return (
       <div>
         <h3>Edit a Stream</h3>
