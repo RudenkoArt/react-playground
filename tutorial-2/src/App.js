19f7324cf13b791bf2f77884b5a305a88c8fe506
RudenkoArt <rudnkoart@gmail.com>
2023-01-30 10:42:05 +0200

    finished section 4

--- tutorial-2/src/App.js
+++ tutorial-2/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns';
+import Setup from './tutorial/4-forms/setup/2-multiple-inputs';
 
 const App = () => {
   return (
     <div className='container'>
-      <h2>
-        <Setup />
-      </h2>
+      <Setup />
     </div>
   );
 };
--- tutorial-2/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ tutorial-2/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,30 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text, setText] = useState('');
+  const [isError, setIsError] = useState(false);
+
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+    <div>
+      {/* <h1>{firstValue}</h1>
+      <h1>value: {secondValue}</h1> */}
+      <h1>{text || 'jonh doe'}</h1>
+      <button className='btn' onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {isError && <h1>error...</h1>}
+      {isError ? (
+        <h1>there is an error!!!</h1>
+      ) : (
+        <div>
+          <h1>there is no error</h1>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ShortCircuit;
