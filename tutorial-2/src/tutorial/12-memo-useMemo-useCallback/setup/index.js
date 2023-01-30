38aac02fed575e6884b56fca71f4b1e61ee7ec7a
RudenkoArt <rudnkoart@gmail.com>
2023-01-30 14:19:36 +0200

    finished section 12

--- tutorial-2/src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ tutorial-2/src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -1,15 +1,38 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react'
-import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import { useFetch } from '../../9-custom-hooks/setup/2-useFetch';
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/javascript-store-products'
+const url = 'https://course-api.com/javascript-store-products';
 
 // every time props or state changes, component re-renders
 
 const Index = () => {
-  const { products } = useFetch(url)
-  const [count, setCount] = useState(0)
+  const { products } = useFetch(url);
+  const [count, setCount] = useState(0);
+  const [cart, setCart] = useState(0);
+
+  const calculateMostExpensive = (data) => {
+    console.log('hello');
+    return (
+      data.reduce((total, item) => {
+        const price = item.fields.price;
+
+        if (price >= total) total = price;
+
+        return total;
+      }, 0) / 100
+    );
+  };
+
+  const addToCart = useCallback(() => {
+    setCart(cart + 1);
+  }, [cart]);
+
+  const mostExpensive = useMemo(
+    () => calculateMostExpensive(products),
+    [products]
+  );
 
   return (
     <>
@@ -17,32 +40,45 @@ const Index = () => {
       <button className='btn' onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1 style={{ marginTop: '3rem' }}>cart : {cart}</h1>
+      <h1>Most Expensive: ${mostExpensive}</h1>
+      <BigList addToCart={addToCart} products={products} />
     </>
-  )
-}
+  );
+};
 
-const BigList = ({ products }) => {
+const BigList = React.memo(({ products, addToCart }) => {
+  useEffect(() => {
+    console.log('big list call');
+  });
   return (
     <section className='products'>
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return (
+          <SingleProduct
+            addToCart={addToCart}
+            key={product.id}
+            {...product}
+          ></SingleProduct>
+        );
       })}
     </section>
-  )
-}
+  );
+});
 
-const SingleProduct = ({ fields }) => {
-  let { name, price } = fields
-  price = price / 100
-  const image = fields.image[0].url
+const SingleProduct = ({ fields, addToCart }) => {
+  console.count('single item called');
+  let { name, price } = fields;
+  price = price / 100;
+  const image = fields.image[0].url;
 
   return (
     <article className='product'>
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick={addToCart}>add to cart</button>
     </article>
-  )
-}
-export default Index
+  );
+};
+export default Index;
